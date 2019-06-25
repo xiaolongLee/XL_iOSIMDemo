cee6051fef72edf4f1ad10bb4b1f5d4890613064
Mac-Qke <mac-qke@Mac-QkedeMac-mini.local>
2019-06-25 10:27:41 +0800

    即时通信Demo

---
+++ iOS_IMDemo/iOS_IMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_IMDemo
+//
+//  Created by Mac-Qke on 2019/6/25.
+//  Copyright © 2019 Mac-Qke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS_IMDemo/iOS_IMDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOS_IMDemo
+//
+//  Created by Mac-Qke on 2019/6/25.
+//  Copyright © 2019 Mac-Qke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
